d0d30d57cadc5fde56269ab7cbca56a1fbb46ea7
ShopyK4 <s0941692@gmail.com>
2025-03-07 12:53:30 -0500

    Entrega clase

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -10,21 +10,24 @@ package ui; // No olvidar especificar el paquete / carpeta de nuestro programa
 import java.util.Scanner; // Para poder usar Scanner, debemos importarlo
 
 public class AtlasPuntuaciones {
+	static Scanner escaner = new Scanner(System.in);
 
 	// Declaración del Escaner que nos ayudará en todo el programa
-	private Scanner escaner;
 
 	// Declaración de nuestras constantes a utilizar
 
-
-	private AtlasPuntuaciones() {
-		escaner = new Scanner(System.in);
-	}
-	
 	
 	public void run()
 	{
 		// Declaracion de todas las variables a usar en el programa
+		
+		System.out.println("Ingrese la cantidad de jugadores: ");
+		int cantidadJugadores = escaner.nextInt();
+		System.out.println("Ingrese la cantidad de rondas: ");
+		int cantidadRondas = escaner.nextInt();
+		int [] jugadores = arregloJugadores(cantidadJugadores);
+		jugadores = puntajeRondas(jugadores, cantidadRondas);
+		resultadoTorneo(jugadores, cantidadRondas);
 
 
 		// Notificacion al usuario de la solicitud de dato
@@ -51,29 +54,57 @@ public class AtlasPuntuaciones {
 	 * @param int[] numeros
 	 * @return 
 	 */
-    public int calcularSumaTotal(int[] numeros) {
-        // Completar
+
+
+	public static int[] arregloJugadores(int cantidadJugadores) {
+		int [] jugadores = new int [cantidadJugadores];
+		return jugadores;
+
+	} 
+	public static int[] puntajeRondas(int[] jugadores, int cantidadRondas) {
+     for (int i = 0; i < jugadores.length; i++) { 
+        jugadores[i] = registrarPuntajePorRondas(i, cantidadRondas);
+    }
+    return jugadores;
+
+}
+	public static int registrarPuntajePorRondas(int jugador, int cantidadRondas) {
+    	int puntajeTotal = 0;
+    for (int i = 0; i < cantidadRondas; i++) { 
+        System.out.println("Ingrese el puntaje del jugador " + (jugador + 1) + " en la ronda " + (i + 1) + ": ");
+        puntajeTotal += escaner.nextInt();
+
+    }
+	metodoPromedio(puntajeTotal, cantidadRondas);
+    return puntajeTotal;
+}
+	public static void resultadoTorneo(int[] jugadores, int cantidadRondas) {
+        System.out.println("Resultados del torneo:");
+		int jugadorGanador = puntuacionAlta(jugadores);
+        for (int i = 0; i < jugadores.length; i++) { 
+            double promedio = metodoPromedio(jugadores[i], cantidadRondas);
+            System.out.println("El puntaje total del jugador " + (i + 1) + " es: " + jugadores[i] +" El promedio por ronda: " + promedio);
+        }
+		System.out.println("El jugador con la puntuacion mas alta es: " + jugadorGanador);
     }
 
-	/**
-	 * Descripción: El método calcularPromedio permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public double calcularPromedio(int[] numeros) {
-        // Completar
+ 	public static double metodoPromedio(double puntTotal, int cantidadRondas) {
+		double resultado = (puntTotal / cantidadRondas);
+        return resultado;
     }
 
-	/**
-	 * Descripción: El método encontrarMayor permite ...
-	 * @param double[] numeros
-	 * @return 
-	 */
-    public double encontrarMayor(double[] numeros) {
-        // Completar
-    }
+	public static int puntuacionAlta(int[] jugadores) {
+		int mayor = 0;
+		int jugadorGanador = 1; 
+		for(int i = 0; i < jugadores.length; i++) {
+			if(jugadores[i] > mayor) {
+				mayor = jugadores[i];
+				jugadorGanador = i + 1; 
+			}
+		}
+		return jugadorGanador;
+	}
 
-	
 
 	
 }
